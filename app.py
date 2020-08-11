233fe48182e6b2ec19c519774958abe0a181190b
Marta Pancaldi <marta.panc@gmail.com>
2020-08-11 20:08:56 +0100

    Test sqlite cache

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
+import os
+
 import requests
 import requests_cache
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, url_for
+from dotenv import load_dotenv
+from pathlib import Path
+
+# Setup functions to read from .env file
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
 
 app = Flask(__name__)
+requests_cache.install_cache(cache_name='openweathermap_cache', backend='sqlite', expire_after=600)
 
 
 @app.route('/')
@@ -13,7 +23,8 @@ def hello_world():
 @app.route('/weather', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        url = "https://api.openweathermap.org/data/2.5/onecall?lat=53.480720&lon=-2.240810&APPID={key}&units=metric"
+        url = "https://api.openweathermap.org/data/2.5/onecall?lat=53.480720&lon=-2.240810&APPID={key}" \
+              "&units=metric".format(key=os.getenv('API_KEY'))
         response_dict = requests.get(url).json()
         return jsonify(response_dict)
     return jsonify({"message": "Hello world!"})
