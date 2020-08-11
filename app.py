bad695f453fc2b27c3d63e7e1eadf525b86c22e0
Marta Pancaldi <marta.panc@gmail.com>
2020-08-11 22:10:29 +0100

    Add Alerts endpoint

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ load_dotenv(dotenv_path=env_path)
 
 OPEN_WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}" \
                        "&APPID={key}&units={units}"
+WEATHERBIT_API_URL = "https://api.weatherbit.io/v2.0/alerts?lat={lat}&lon={lon}&key={key}"
 
 app = Flask(__name__)
 CORS(app)
@@ -28,13 +29,34 @@ def hello_world():
     return jsonify({"server status": "UP"})
 
 
+@app.route('/alerts', methods=['GET'])
+def alerts():
+    errors = []
+    check_empty_params(errors)
+    if len(errors) > 0:
+        return jsonify({"errors": errors})
+
+    latitude = request.args.get('lat')
+    longitude = request.args.get('lon')
+
+    check_invalid_params(errors, latitude, longitude)
+
+    if len(errors) > 0:
+        return jsonify({"errors": errors})
+
+    url = WEATHERBIT_API_URL.format(lat=latitude, lon=longitude, key=os.getenv("WB_API_KEY"))
+
+    response_dict = requests.get(url).json()
+    for alert in response_dict['alerts']:
+        head, sep, _ = alert['description'].partition('.')
+        alert['description'] = head + sep
+    return jsonify(response_dict)
+
+
 @app.route('/onecall', methods=['GET'])
 def one_call():
     errors = []
-    if 'lat' not in request.args:
-        errors.append("Missing latitude")
-    if 'lon' not in request.args:
-        errors.append("Missing longitude")
+    check_empty_params(errors)
 
     if len(errors) > 0:
         return jsonify({"errors": errors})
@@ -43,20 +65,31 @@ def one_call():
     longitude = request.args.get('lon')
     units = request.args.get('units') if 'units' in request.args else 'default'
 
-    if not validate_latitude(latitude):
-        errors.append("Invalid latitude")
-    if not validate_longitude(longitude):
-        errors.append("Invalid longitude")
+    check_invalid_params(errors, latitude, longitude)
     if not validate_units(units):
         errors.append("Invalid units")
 
     if len(errors) > 0:
         return jsonify({"errors": errors})
 
-    url = OPEN_WEATHER_API_URL.format(lat=latitude, lon=longitude, units=units, key=os.getenv("API_KEY"))
+    url = OPEN_WEATHER_API_URL.format(lat=latitude, lon=longitude, units=units, key=os.getenv("OWM_API_KEY"))
 
     response_dict = requests.get(url).json()
     return jsonify(response_dict)
+
+
+def check_empty_params(errors):
+    if 'lat' not in request.args:
+        errors.append("Missing latitude")
+    if 'lon' not in request.args:
+        errors.append("Missing longitude")
+
+
+def check_invalid_params(errors, latitude, longitude):
+    if not validate_latitude(latitude):
+        errors.append("Invalid latitude")
+    if not validate_longitude(longitude):
+        errors.append("Invalid longitude")
 
 
 if __name__ == '__main__':
