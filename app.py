ee0391844eb8a9bdd1d936b2f3a3cec1694d76da
Marta Pancaldi <marta.panc@gmail.com>
2020-08-11 19:29:34 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+import requests
+import requests_cache
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/weather', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        url = "https://api.openweathermap.org/data/2.5/onecall?lat=53.480720&lon=-2.240810&APPID={key}&units=metric"
+        response_dict = requests.get(url).json()
+        return jsonify(response_dict)
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
